65625231e4f0ebc56dc7e1d56289326ad92b9656
Aisha Abdq <Ashmq22@gmail.com>
2021-12-19 15:07:40 +0300

    North West East South Ass

--- NESWAss/ViewController.swift
+++ NESWAss/ViewController.swift
@@ -14,6 +14,39 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func northBut(_ sender: UIButton) {
+        let vc = storyboard?.instantiateViewController(withIdentifier: "secondVC") as! SecondViewController
+        vc.direction = "North"
+       // self.navigationController?.pushViewController(vc, animated: true)
+        vc.modalPresentationStyle = .fullScreen
+        present(vc, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func eastBut(_ sender: UIButton) {
+        let vc = storyboard?.instantiateViewController(withIdentifier: "secondVC") as! SecondViewController
+        vc.direction = "East"
+        //self.navigationController?.pushViewController(vc, animated: true)
+        vc.modalPresentationStyle = .fullScreen
+        present(vc, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func southBut(_ sender: UIButton) {
+        let vc = storyboard?.instantiateViewController(withIdentifier: "secondVC") as! SecondViewController
+        vc.direction = "South"
+      //  self.navigationController?.pushViewController(vc, animated: true)
+        vc.modalPresentationStyle = .fullScreen
+        present(vc, animated: true, completion: nil)
+    }
+    @IBAction func westBut(_ sender: UIButton) {
+        let vc = storyboard?.instantiateViewController(withIdentifier: "secondVC") as! SecondViewController
+        vc.direction = "West"
+       // self.navigationController?.pushViewController(vc, animated: true)
+        vc.modalPresentationStyle = .fullScreen
+        present(vc, animated: true, completion: nil)
+    }
+    
+    
 }
 
